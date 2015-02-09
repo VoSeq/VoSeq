96f303f3a89d0e5e4c29802ba92ecfb50ae1d440
mezarino <vm_solism@yahoo.es>
2015-02-09 03:16:43 +0100

    correct field name to author

--- voseq/public_interface/models.py
+++ voseq/public_interface/models.py
@@ -127,7 +127,7 @@ class Vouchers(models.Model):
     country = models.CharField(max_length=100, blank=True)
     specificLocality = models.CharField(max_length=250, help_text="Locality of origin for this specimen.", blank=True)
     typeSpecies = models.CharField(max_length=1, choices=TYPE_SPECIES_CHOICES,
-                                   help_text="Is this a type species?", blank=True)
+                                   help_text="Is this a type species?")
     latitude = models.FloatField(blank=True, null=True)
     longitude = models.FloatField(blank=True, null=True)
     max_altitude = models.IntegerField(blank=True, null=True, help_text="Enter altitude in meters above sea level.")
